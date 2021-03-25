8532faeb1b93d20bb6dfbbf6b505242e9201ecbe
Tucker Hoog <tuckerhoog@tutanota.com>
2021-03-25 18:00:28 -0400

    completed testing on didUserWin function

--- test/example.test.js
+++ test/example.test.js
@@ -8,15 +8,28 @@ const test = QUnit.test;
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
-    const drawOne = didUserWin('');
-
+    const drawOne = didUserWin('rock', 'rock');
+    const drawTwo = didUserWin('rock', 'paper');
+    const drawThree = didUserWin('rock', 'scissors');
+    const drawFour = didUserWin('paper', 'paper');
+    const drawFive = didUserWin('paper', 'rock');
+    const drawSix = didUserWin('paper', 'scissors');
+    const drawSeven = didUserWin('scissors', 'rock');
+    const drawEight = didUserWin('scissors', 'paper');
+    const drawNine = didUserWin('scissors', 'scissors');
 
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(didUserWin('test', 'test'), expected);
+    expect.equal(drawOne, 'tie');
+    expect.equal(drawTwo, 'lose');
+    expect.equal(drawThree, 'win');
+    expect.equal(drawFour, 'tie');
+    expect.equal(drawFive, 'win');
+    expect.equal(drawSix, 'lose');
+    expect.equal(drawSeven, 'lose');
+    expect.equal(drawEight, 'win');
+    expect.equal(drawNine, 'tie');
 });
