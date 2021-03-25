fd349d0b38603d69cc26838efc669e809ab2ae94
Tucker Hoog <tuckerhoog@tutanota.com>
2021-03-25 18:50:46 -0400

    got users input from radio button and started displaying results on page

--- app.js
+++ app.js
@@ -7,7 +7,6 @@
 import { didUserWin } from './utils.js';
 import { getRandomThrow } from './get-random-throw.js';
 
-const usersPick = document.querySelector('input:checked');
 const button = document.getElementById('button');
 const playersPickDisplay = document.getElementById('player-pick-display');
 const aiPickDisplay = document.getElementById('AI-pick-display');
@@ -15,7 +14,27 @@ const resultPickDisplay = document.getElementById('result-display');
 const winsDisplau = document.getElementById('wins-display');
 const tiesDisplau = document.getElementById('ties-display');
 const lossesDisplau = document.getElementById('losses-display');
+let wins = 0;
+let losses = 0;
+let ties = 0;
 
 button.addEventListener('click', () => {
+    const usersPick = document.querySelector(':checked');
+    const userThrow = usersPick.value;
+    const aiThrow = getRandomThrow();
+
+    const result = didUserWin(userThrow, aiThrow);
+
+    playersPickDisplay.textContent = userThrow.value;
+
+    if (result === 'win') {
+        wins++;
+    }
+    if (result === 'lose') {
+        losses++;
+    }
+    if (result === 'tie') {
+        ties++;
+    }
 
 });
