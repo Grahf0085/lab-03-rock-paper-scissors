48a430e651f94617493054c621906029da8df0d6
Tucker Hoog <tuckerhoog@tutanota.com>
2021-03-25 18:22:50 -0400

    added DOM elements to app.js

--- app.js
+++ app.js
@@ -4,3 +4,18 @@
 
 // set event listeners to update state and DOM
 
+import { didUserWin } from './utils.js';
+import { getRandomThrow } from './get-random-throw.js';
+
+const usersPick = document.querySelector('input:checked');
+const button = document.getElementById('button');
+const playersPickDisplay = document.getElementById('player-pick-display');
+const aiPickDisplay = document.getElementById('AI-pick-display');
+const resultPickDisplay = document.getElementById('result-display');
+const winsDisplau = document.getElementById('wins-display');
+const tiesDisplau = document.getElementById('ties-display');
+const lossesDisplau = document.getElementById('losses-display');
+
+button.addEventListener('click', () => {
+
+});
