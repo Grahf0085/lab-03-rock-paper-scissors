3c84f207d572fce491ec61abf1f97ed45df1c637
Tucker Hoog <tuckerhoog@tutanota.com>
2021-03-25 20:24:46 -0400

    added styling and reset button

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
-// import functions and grab DOM elements
-
-// initialize state
-
-// set event listeners to update state and DOM
-
 import { didUserWin } from './utils.js';
 import { getRandomThrow } from './get-random-throw.js';
 
 const button = document.getElementById('button');
+const resetButton = document.getElementById('reset');
 const playersPickDisplay = document.getElementById('player-pick-display');
 const aiPickDisplay = document.getElementById('AI-pick-display');
 const resultPickDisplay = document.getElementById('result-display');
 const winsDisplay = document.getElementById('wins-display');
 const tiesDisplay = document.getElementById('ties-display');
 const lossesDisplay = document.getElementById('losses-display');
+const totalDisplay = document.getElementById('total-display');
+
 let wins = 0;
 let losses = 0;
 let ties = 0;
+let total = 0;
 
 button.addEventListener('click', () => {
-    const usersPick = document.querySelector(':checked');
+    const usersPick = document.querySelector('input:checked');
     const userThrow = usersPick.value;
     const aiThrow = getRandomThrow();
-
     const result = didUserWin(userThrow, aiThrow);
 
     playersPickDisplay.textContent = userThrow;
     aiPickDisplay.textContent = aiThrow;
     resultPickDisplay.textContent = result;
+
+    total++;
 
     if (result === 'win') {
         wins++;
@@ -40,7 +39,19 @@ button.addEventListener('click', () => {
     if (result === 'tie') {
         ties++;
         tiesDisplay.textContent = ties;
-
     }
 
+    totalDisplay.textContent = total;
+});
+
+resetButton.addEventListener('click', () => {
+    wins = 0;
+    losses = 0;
+    ties = 0;
+    total = 0;
+
+    winsDisplay.textContent = wins;
+    lossesDisplay.textContent = losses;
+    tiesDisplay.textContent = ties;
+    totalDisplay.textContent = total;
 });
